8077dde23dc15ce887cf709236a2f15c714fe11e
Casey Cross <hellocaseycross@gmail.com>
2016-11-05 23:50:22 -0600

    Some styling adjustments

--- components/Text.jsx
+++ components/Text.jsx
@@ -11,13 +11,19 @@ export default class Text extends Component {
   //     };
   //   }
   //
-  // componentDidMount() {
-  //   this.setState(
-  //     { allVoices: speechSynthesis.getVoices() }
-  //   );
-  // }
+  componentDidMount() {
+    this.createVoiceList();
+  }
 
   createVoiceList() {
+    console.log('is anything happening?');
+    // return (
+      // <select name="voices" className="voices">
+      //   <option value="Whisper">Whisper</option>
+      //   <option value="Sara">Sara</option>
+      //   <option value="Zarvox">Zarvox</option>
+      // </select>
+    // )
     // this.allVoices.map(voice => {
     //   <option value={voice.name}>{voice.name}</option>
     // });
@@ -25,9 +31,11 @@ export default class Text extends Component {
     //   console.log(voice.name);
     //   // return (<option value={voice.name}>{voice.name}</option>);
     // });
-    React.Children.map(window.speechSynthesis.getVoices(), (voice) => {
+    // debugger;
+    let something = window.speechSynthesis.getVoices();
 
-    console.log(voice.name);
+    React.Children.map(something, () => {
+      console.log(voice.name);
     });
   }
 
@@ -36,15 +44,16 @@ export default class Text extends Component {
     let pageId = this.props.textKey;
 
     var msg = new SpeechSynthesisUtterance(storyText[pageId].pageText);
-    // var voices = window.speechSynthesis.getVoices();
-    // msg.voice = voices[0];
     // msg.voice = speechSynthesis.getVoices().filter((voice) => {
     //   return voice.name = document.querySelector('voices').value;
     // });
-    msg.voice = speechSynthesis.getVoices().filter((voice) => {
-       return voice.name === 'Whisper';
+
+
+    msg.voice = window.speechSynthesis.getVoices().filter((voice) => {
+       return voice.name === 'Bahh';
+      //  return voice.name === document.querySelector('.voices').val();
      })[0];
-    // debugger;
+
     speechSynthesis.speak(msg);
 
   //   <Speech
@@ -55,23 +64,29 @@ export default class Text extends Component {
 //   speechSynthesis.getVoices().forEach(function(voice) {
 //   console.log(voice.name, voice.default ? '(default)' :'');
 // });
-//
-//
-// m
-// speechSynthesis.speak(msg);
 
-//
-//
   render () {
     let pageId = this.props.textKey;
 
     return (
       <section className="story">
-        <p>{storyText[pageId].pageText}</p>
+        <p className="printed-text">{storyText[pageId].pageText}</p>
         <div className="read-aloud">
-          <label>Choose a voice
+          <label className="choices">Choose a voice:
             <select name="voices" className="voices">
-              {this.createVoiceList()}
+              <option value="Whisper">Whisper</option>
+              <option value="Bahh">Bahh</option>
+              <option value="Boing">Boing</option>
+              <option value="Bubbles">Bubbles</option>
+              <option value="Good News">Good News</option>
+              <option value="Pipe Organ">Pipe Organ</option>
+              <option value="Princess">Princess</option>
+              <option value="Tessa">Tessa</option>
+              <option value="Ting-Ting">Ting-Ting</option>
+              <option value="Trinoids">Trinoids</option>
+              <option value="Google US English">Google US English</option>
+              <option value="Google UK English Male">Google UK English Male</option>
+              <option value="Google español">Google español</option>
             </select>
           </label>
           <Button
@@ -79,7 +94,7 @@ export default class Text extends Component {
             handleClick={(e) => this.textToSpeech(e)}
             text="Read Aloud"
           />
-    </div>
+        </div>
       </section>
     );
   }
